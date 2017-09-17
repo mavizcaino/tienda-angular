3c6e7737103fdb68d6d44df3911b26512d6b51f4
mavizcaino <32041795+mavizcaino@users.noreply.github.com>
2017-09-17 21:13:58 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+
+//rutas
+import { routing, appRoutingProviders } from './app.routing';
+
+//componentes
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
